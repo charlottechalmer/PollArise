c2381bdcdc23f508138a530f23bf066388cacd1e
charlottechalmer <charlotte.taylor7693@gmail.com>
2016-11-16 12:40:10 -0700

    adjusting dynamic color overlay function

--- app/public/js/page.js
+++ app/public/js/page.js
@@ -39,34 +39,30 @@ function displayCelebrity(celebrity) {
   var party = celebrity.user.politicalParty;
   var colorText = $(".cel_name, .matchdiv a, .matchdiv p");
   var colorElem = $(".left-bar, .celebrity .third-border, .celebrity .avatar, .celebrityp");
+  var overlay = $('.overlay');
     if (party === "Republican") {
       colorText.css({color: "#D40000"});
-      colorElem.css({backgroundColor: "D40000", color: "D40000", borderColor: "D40000"});  
+      colorElem.css({backgroundColor: "D40000", color: "D40000", borderColor: "D40000"});
+      overlay.addClass('overlay-red'); 
     } else if (party === "Democratic") {
       colorText.css({color: "0098ff"});
       colorElem.css({backgroundColor: "0098ff", color: "0098ff", borderColor: "0098ff"});
+      overlay.addClass('overlay-blue');
     } else if (party === "Libertarian") {
       colorText.css({color: "FFCB00"});
-      colorElem.css({backgroundColor: "FFCB00", color: "FFCB00", borderColor: "FFCB00"});
+      colorElem.css({backgroundColor: "FFCB00", color: "FFCB00", borderColor: "FFCB00"})
+      overlay.addClass('overlay-yellow');;
     } else if (party === "Green Party") {
       colorText.css({color: "038F19"});
       colorElem.css({backgroundColor: "038F19", color: "038F19", borderColor: "038F19"});
+      overlay.addClass('overlay-green');
     } else if (party === "Independent") {
       colorText.css({color: "3D3D3D"});
       colorElem.css({backgroundColor: "3D3D3D", color: "3D3D3D", borderColor: "3D3D3D"});
+      overlay.addClass('overlay-grey');
+
     }
-    var overlay = $('.overlay');
-      if (party === 'Republican') {
-        overlay.addClass('overlay-red');
-      } else if (party === 'Democratic') {
-        overlay.addClass('overlay-blue');
-      } else if (party === 'Libertarian') {
-        overlay.addClass('overlay-yellow');
-      } else if (party === 'Green Party') {
-        overlay.addClass('overlay-green');
-      } else if (party === 'Independent') {
-        overlay.addClass('overlay-grey');
-      }
+
   // Update traits
   var idType;
   if (currentType === PERSONALITY) idType = '#personality_trait_';
